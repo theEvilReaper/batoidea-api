1d7b78140b8381b8635b093475fb08d863d8c7b0
theEvilReaper <steffenwx@gmail.com>
2021-08-02 15:57:26 +0200

    Add redis event

---
+++ src/main/java/net/theEvilReaper/bot/api/database/REvent.java
@@ -0,0 +1,22 @@
+package net.theEvilReaper.bot.api.database;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The {@link REvent} is the base class for the event system in redis. An event should need a topic.
+ * The topic is the identifier for the event.
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+
+public abstract class REvent {
+
+    /**
+     * Returns the topic from the {@link REvent}.
+     * @return the given topic
+     */
+
+    @NotNull
+    public abstract String getTopic();
+}
