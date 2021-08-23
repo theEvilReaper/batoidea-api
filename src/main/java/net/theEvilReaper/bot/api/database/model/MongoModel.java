b0d416239aac3ad59c35618cce8da88793d1e663
theEvilReaper <steffenwx@gmail.com>
2021-08-23 22:21:23 +0200

    Improve database backend

---
+++ src/main/java/net/theEvilReaper/bot/api/database/model/MongoModel.java
@@ -0,0 +1,9 @@
+package net.theEvilReaper.bot.api.database.model;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+
+public interface MongoModel { }
---
+++ src/main/java/net/theEvilReaper/bot/api/database/model/RedisModel.java
@@ -0,0 +1,10 @@
+package net.theEvilReaper.bot.api.database.model;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+
+public interface RedisModel {
+}
