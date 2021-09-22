e21f8f6ea5e558c6a270c83a62a0ca4577c28408
theEvilReaper <steffenwx@gmail.com>
2021-09-22 20:01:47 +0200

    Add some basic documentation

--- src/main/java/net/theEvilReaper/bot/api/interaction/InteractionType.java
+++ src/main/java/net/theEvilReaper/bot/api/interaction/InteractionType.java
@@ -13,12 +13,19 @@ public enum InteractionType {
     SERVER((byte)2),
     CHANNEL((byte)3);
 
+    //Reduces the defensive copies of the array when calls the values[] method
     private static final InteractionType[] VALUES = values();
+
     private final byte id;
 
     InteractionType(byte id) {
         this.id = id;
     }
+
+    /**
+     * Returns the id from the {@link InteractionType}.
+     * @return  the given id
+     */
 
     public int getId() {
         return id;
