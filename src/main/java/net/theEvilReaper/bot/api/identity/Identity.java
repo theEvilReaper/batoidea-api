5422dbe5b5e327a4a4372535a8ee66e39900db6b
theEvilReaper <steffenwx@gmail.com>
2021-10-03 18:51:31 +0200

    Add some missing documentation

--- src/main/java/net/theEvilReaper/bot/api/identity/Identity.java
+++ src/main/java/net/theEvilReaper/bot/api/identity/Identity.java
@@ -4,13 +4,13 @@ import com.github.manevolent.ts3j.identity.LocalIdentity;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * Contains the methods needed to load or change an identity (TS) of a bot.
  * @author theEvilReaper
  * @version 1.0.0
  * @since 1.0.0
  **/
 
 public interface Identity {
-
 
     /**
      * Sets / overwrites the underlying {@link LocalIdentity}.
