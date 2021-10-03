850427cd2e9a4a755ab0b356d4642dfad8a04747
theEvilReaper <steffenwx@gmail.com>
2021-10-03 18:31:12 +0200

    Add some missing documentation

--- src/main/java/net/theEvilReaper/bot/api/command/CommandExecutor.java
+++ src/main/java/net/theEvilReaper/bot/api/command/CommandExecutor.java
@@ -7,12 +7,20 @@ import org.jetbrains.annotations.Nullable;
 import java.util.logging.Logger;
 
 /**
+ * Holds only a method to execute the implementation of an command.
  * @author theEvilReaper
  * @version 1.0.0
  * @since 1.0.0
  **/
 
 public interface CommandExecutor {
+
+    /**
+     * The method holds the logic for each command implementation of the bot.
+     * @param executor The client who executed the command
+     * @param command The command itself
+     * @param args The arguments from the command
+     */
 
     void onCommand(@NotNull Client executor, @NotNull String command, @Nullable String... args);
 }
