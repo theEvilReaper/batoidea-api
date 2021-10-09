cb1e5f297c69abe6e8f1addc9041ee39514b3040
theEvilReaper <steffenwx@gmail.com>
2021-10-09 19:46:44 +0200

    #3 Add CommandCallback class

---
+++ src/main/java/net/theEvilReaper/bot/api/command/CommandCallback.java
@@ -0,0 +1,24 @@
+package net.theEvilReaper.bot.api.command;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Functional interface used by the {@link CommandManager}
+ * to execute a callback if an unknown command is run.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+@FunctionalInterface
+public interface CommandCallback {
+
+    /**
+     * Executed when a {@link CommandSender} run's an unknown command
+     * @param commandSender The {@link CommandSender} who executed the command
+     * @param command The command as string
+     */
+
+    void apply(@NotNull CommandSender commandSender, @NotNull String command);
+}
