255bab0cbfac4f54b70e080b30ed0bd4c82c2a1d
theEvilReaper <steffenwx@gmail.com>
2021-10-13 23:17:13 +0200

    Add own interaction for the permission handling

---
+++ src/main/java/net/theEvilReaper/bot/api/interaction/PermissionInteraction.java
@@ -0,0 +1,30 @@
+package net.theEvilReaper.bot.api.interaction;
+
+import com.github.manevolent.ts3j.api.Permission;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+
+//TODO: Add method for the permission handling with channel or own class?
+public interface PermissionInteraction {
+
+    /**
+     * Add a new single permission or an array with permission to the given client.
+     * @param clientDatabaseID The client id from the {@link com.github.manevolent.ts3j.api.Client}
+     * @param permissions The permission(s) to add
+     */
+
+    void addPermission(int clientDatabaseID, @Nullable Permission... permissions);
+
+    /**
+     * Add a new single permission or an array with permission to the given client.
+     * @param clientDatabaseID The client id from the {@link com.github.manevolent.ts3j.api.Client}
+     * @param permissions The permission(s) to add
+     */
+    void removePermission(int clientDatabaseID, @Nullable Permission... permissions);
+
+}
