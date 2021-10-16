f5fa90e6e9e14063b2b64461342c6b74b3e7fd85
theEvilReaper <steffenwx@gmail.com>
2021-10-16 22:53:48 +0200

    Work on the structure of the RunningService

--- src/main/java/net/theEvilReaper/bot/api/service/RunningService.java
+++ src/main/java/net/theEvilReaper/bot/api/service/RunningService.java
@@ -13,17 +13,28 @@ public abstract class RunningService extends StaticService implements Runnable {
     protected final Object lock;
 
     private boolean running;
+    private boolean stopping;
 
-    public RunningService(@NotNull String name) {
+    private long interval;
+
+    public RunningService(@NotNull String name, long interval) {
         super(name);
+        this.interval = interval;
         this.lock = new Object();
     }
+
+    protected void init() {}
 
     public abstract void onStart();
 
     public abstract void onUpdate();
 
     public abstract void onStop();
+
+    public void start() {
+        if (running) return;
+        this.onStart();
+    }
 
     public void stop() {
         if (!running) return;
@@ -33,6 +44,48 @@ public abstract class RunningService extends StaticService implements Runnable {
             running = false;
             onStop();
         }
+    }
 
+    @Override
+    public void run() {
+        if (running) return;
+
+        running = true;
+
+        init();
+
+        while (!stopping && !Thread.interrupted()) {
+            var now = System.currentTimeMillis();
+            try {
+                this.onUpdate();
+            } catch (Exception exception) {
+                break;
+            }
+
+            if (stopping || Thread.interrupted()) {
+                break;
+            }
+
+            synchronized (lock) {
+                try {
+                    lock.wait(Math.max(10, interval - (System.currentTimeMillis() - now)));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        onStop();
+
+        running = false;
+        stopping = false;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public boolean isStopping() {
+        return stopping;
     }
 }
