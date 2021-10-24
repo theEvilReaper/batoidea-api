5e184a604cb05dd98c89a8de2726d6c5a3172758
theEvilReaper <steffenwx@gmail.com>
2021-10-24 19:28:42 +0200

    Remove a value and add some missing documentation

--- src/main/java/net/theEvilReaper/bot/api/command/Command.java
+++ src/main/java/net/theEvilReaper/bot/api/command/Command.java
@@ -4,12 +4,8 @@ import net.theEvilReaper.bot.api.command.condition.CommandCondition;
 import net.theEvilReaper.bot.api.interaction.AbstractInteractionFactory;
 import net.theEvilReaper.bot.api.interaction.InteractionType;
 import net.theEvilReaper.bot.api.interaction.UserInteraction;
-import net.theEvilReaper.bot.api.util.Strings;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 /**
  * The class represents the basic structure for the commands
@@ -23,7 +19,6 @@ public abstract class Command implements CommandExecutor {
     protected UserInteraction userInteraction;
 
     private final String name;
-    private final String[] names;
     private final String[] aliases;
 
     private String usage;
@@ -39,18 +34,12 @@ public abstract class Command implements CommandExecutor {
         this.userInteraction = factory.getInteraction(InteractionType.CLIENT, UserInteraction.class);
         this.name = name;
         this.aliases = aliases;
-        this.names = Stream.concat(Arrays.stream(aliases), Stream.of(name)).toArray(String[]::new);
     }
 
-    public Command(@NotNull String name, @Nullable String... aliases) {
-        this.name = name;
-        this.aliases = aliases;
-        this.names = Stream.concat(Arrays.stream(aliases), Stream.of(name)).toArray(String[]::new);
-    }
-
-    public Command(@NotNull String name) {
-        this(name, Strings.EMPTY_ARRAY);
-    }
+    /**
+     * Set a given {@link CommandCondition} to the command.
+     * @param condition The {@link CommandCondition} to set
+     */
 
     public void setCondition(@Nullable CommandCondition condition) {
         this.condition = condition;
@@ -103,9 +92,5 @@ public abstract class Command implements CommandExecutor {
     @Nullable
     public String[] getAliases() {
         return aliases;
-    }
-
-    public String[] getNames() {
-        return names;
     }
 }
