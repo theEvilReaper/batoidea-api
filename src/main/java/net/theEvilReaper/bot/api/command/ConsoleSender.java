4180c3f7b87f840021ae18ba6925c6078e659c40
theEvilReaper <steffenwx@gmail.com>
2021-12-03 20:43:00 +0100

    Remove deprecated method

--- src/main/java/net/theEvilReaper/bot/api/command/ConsoleSender.java
+++ src/main/java/net/theEvilReaper/bot/api/command/ConsoleSender.java
@@ -5,25 +5,30 @@ import org.jetbrains.annotations.NotNull;
 import java.util.logging.Logger;
 
 /**
+ * The class represents the implementation for the {@link ConsoleSender}.
  * @author theEvilReaper
  * @version 1.0.0
- * @since
+ * @since 1.0.0
  **/
 
 public class ConsoleSender implements CommandSender {
 
     private static final Logger LOGGER = Logger.getLogger("BotLogger");
 
+    /**
+     * Send as message to the sender.
+     * @param message the message to send
+     */
+
     @Override
     public void sendMessage(@NotNull String message) {
         LOGGER.info(message);
     }
 
-    @Override
-    public boolean isConsole() {
-        return true;
-    }
-
+    /**
+     * Returns the name from the sender.
+     * @return the give name
+     */
     @Override
     public String getName() {
         return "Console";
