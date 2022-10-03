3fea90694ed575dfb70b36211ae766b0d70658f3
theEvilReaper <steffenwx@gmail.com>
2022-10-03 20:45:57 +0200

    Improve RegEx declaration

--- src/main/java/net/theEvilReaper/bot/api/util/Strings.java
+++ src/main/java/net/theEvilReaper/bot/api/util/Strings.java
@@ -28,7 +28,7 @@ public final class Strings {
             Pattern.compile("(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]");
 
     private static final Pattern IPV4_PATTERN =
-            Pattern.compile("^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}$");
+            Pattern.compile("^((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])(\\.(?!$)|$)){4}$");
 
     private Strings() {}
 
